148e0e335f8432a19b46b0004dd9abd0c5eb13b9
Sharonah8 <nyamongosharon88@gmail.com>
2022-11-06 10:07:51 +0300

    Assignment complete

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+    def say_that_thing_you_say
+        Character.first.name + " always says: " + Character.first.catchphrase
+    end
 end
